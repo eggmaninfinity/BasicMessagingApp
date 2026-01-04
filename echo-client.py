36f0123e0527de6c9ceccc74439d39f554a67b8b
eggmaninfinity <joshuathomasrutter@gmail.com>
2026-01-04 08:58:05 +0100

    Initial Commit

---
+++ echo-client.py
@@ -0,0 +1,19 @@
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    print("Connected. Type 'quit' to exit")
+
+    while True:
+        message = input("Message: ")
+
+        if message.lower() == "quit":
+            break
+        
+        s.sendall(message.encode())
+        data = s.recv(1024)
+        print(f"Received: {data.decode()}")
+
---
+++ echo-server.py
@@ -0,0 +1,27 @@
+import socket
+
+HOST = "0.0.0.0"  # listening on all IPv4(?) interfaces
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+
+    with conn:
+        print(f"Connected by {addr}")
+      
+        while True:
+            data = conn.recv(1024)
+
+            if not data:
+                break
+            
+            print(f"Received: {data.decode()}")
+            reply = input(f"Message: ")
+            conn.sendall(reply.encode())
+
+print(f"Connection closed by {addr}")
+
+# TODO: server asks for name on connect, adds name to dict with IP (names deleted on quit?), lists other active users in dict, 
+# client connects by typing in name (how does other client know to listen for conn from other client?)
